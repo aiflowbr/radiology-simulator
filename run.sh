422e6c80f498b4cab45dd8fe6d5eca3e2c7aabd5
Anderson Calixto <andersonbr@gmail.com>
2024-04-15 22:03:28 -0300

    run.sh

--- run.sh
+++ run.sh
@@ -5,4 +5,9 @@
 # export AETITLE ADDRESS AEPORT REMOTE_AETITLE REMOTE_ADDRESS REMOTE_PORT
 # python src/run.py
 
-docker run --rm -it -v /Users/anderson/Dev/MDCC/archive/CHEST_XRAY:/dataset --env-file=.env andersonbr/radiology-simulator python run.py
+# docker run --rm -it -v /Users/anderson/Dev/MDCC/archive/CHEST_XRAY:/dataset --env-file=.env andersonbr/radiology-simulator python run.py
+if [ "`uname`" == "Darwin" ]; then
+    LOCAL_DATASET_PATH=/Users/anderson/Dev/MDCC/archive/CHEST_XRAY docker-compose up
+else
+    LOCAL_DATASET_PATH=/home/anderson/Dev/MDCC/chest_xray docker-compose up
+fi
